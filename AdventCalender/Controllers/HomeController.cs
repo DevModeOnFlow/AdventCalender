2668f8bbedf8bc3c7ef1d1b6846ca84a202e9363
DevModeOnFlow <primarylogicslayer@gmail.com>
2026-06-18 18:16:20 +0300

    обновлена пагинация

--- AdventCalender/Controllers/HomeController.cs
+++ AdventCalender/Controllers/HomeController.cs
@@ -15,8 +15,9 @@ namespace AdventCalender.Controllers
 
         public async Task<IActionResult> Index(string searchString)
         {
-            var sellersQuery = _context.Users
-                .Where(u => !string.IsNullOrEmpty(u.StoreName));
+            int pageSize = 8; 
+
+            var sellersQuery = _context.Users.Where(u => !string.IsNullOrEmpty(u.StoreName));
 
             if (!string.IsNullOrEmpty(searchString))
             {
@@ -28,8 +29,32 @@ namespace AdventCalender.Controllers
 
             ViewData["CurrentFilter"] = searchString;
 
-            var sellers = await sellersQuery.ToListAsync();
+            int totalItems = await sellersQuery.CountAsync();
+            ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            var sellers = await sellersQuery.Take(pageSize).ToListAsync();
+
             return View(sellers);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoadMore(string searchString, int page)
+        {
+            int pageSize = 8;
+
+            var sellersQuery = _context.Users.Where(u => !string.IsNullOrEmpty(u.StoreName));
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+                sellersQuery = sellersQuery.Where(s =>
+                    s.StoreName.ToLower().Contains(searchString) ||
+                    (s.Description != null && s.Description.ToLower().Contains(searchString)));
+            }
+
+            var sellers = await sellersQuery.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return PartialView("SellerCardList", sellers);
         }
 
         public IActionResult Privacy()
---
+++ AdventCalender/Views/Home/SellerCardList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdventCalender.Views.Home
+{
+    public class SellerCardListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
